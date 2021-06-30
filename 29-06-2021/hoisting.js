37f73ca94065570b5e7e3c77f07ca13d4b2ec16e
tahermamun <rhL201830019@gamail.com>
2021-07-01 00:10:23 +0600

    30-06-2021 code

--- 29-06-2021/hoisting.js
+++ 29-06-2021/hoisting.js
@@ -8,6 +8,7 @@ console.log(x)
 
 // Hoisting  Example 2
 
+
 check()
 function check(){
 console.log('hello world')
@@ -18,10 +19,6 @@ check()
 // Hoisting  Example 2
 
 // console.log(x)
-
-
  let x = 'love'
-
-
 console.log(x)
 
---
+++ 30-06-2021/execution.js
@@ -0,0 +1,25 @@
+var x = 'i love bangladesh'
+var y = 'i love bangladesh'
+
+function check() {
+
+    var str = 'i love bangladesh'
+
+    console.log(str)
+
+    function check2() {
+        var str2 = 'i love bangladesh'
+        console.log(str2)
+    }
+    check2()
+}
+
+console.log(y)
+
+check()
+
+console.log(y)
+console.log(y)
+
+
+
